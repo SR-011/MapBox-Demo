6b31f204e0ba6bdfb182136fcd3ba9d52f58aaeb
sohel.rana <me.sohelmollah@gmail.com>
2021-10-13 16:15:36 +0600

    security

--- app/src/main/java/com/example/mapboxdemo/data/repository/Repository.kt
+++ app/src/main/java/com/example/mapboxdemo/data/repository/Repository.kt
@@ -4,5 +4,5 @@ import com.example.mapboxdemo.data.api.RetrofitBuilder
 
 class Repository {
 
-    suspend fun getWeatherInfo(place:String) = RetrofitBuilder.weatherService.getWeatherInfo(place, "e724aa55752f45f4a9084258210410")
+    suspend fun getWeatherInfo(place:String) = RetrofitBuilder.weatherService.getWeatherInfo(place, "Api Key")
 }
